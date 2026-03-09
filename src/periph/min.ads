4a578b1685bbc6fe03af74df24db7489c5b33f49
jwyffels2 <justinwyffels@gmail.com>
2026-03-09 18:58:58 -0400

    removed C compilation

--- src/periph/min.ads
+++
@@ -1,5 +0,0 @@
--- This is the min protocol "spec file" and defines what procedures/functions are available to use in helios.adb
-package Min is
-    procedure Init;
-    procedure Send_Test;
-end Min;
--- src/periph/test_jpeg.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-extern const uint8_t test_jpeg[];
-extern const uint32_t test_jpeg_len;
